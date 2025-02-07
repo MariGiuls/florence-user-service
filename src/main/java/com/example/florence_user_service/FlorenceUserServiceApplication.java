ad424198eecd98034ce0494a5f847c692bd9cac0
giuliamarinelli <giulia.marinelli@alten.it>
2025-02-07 22:26:24 +0100

    init project

---
+++ src/main/java/com/example/florence_user_service/FlorenceUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.florence_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorenceUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlorenceUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/florence_user_service/FlorenceUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.florence_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorenceUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
